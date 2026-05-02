09ed8830f4960e36925dbda27c1db63177fd7dee
desmond <fuhaddesmond@users.noreply.huggingface.co>
2026-05-02 07:48:47 +0000

    Upload next.config.ts with huggingface_hub

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,9 @@ const nextConfig: NextConfig = {
     "preview-590dd7b5-1e67-4290-99e8-b92fae037b97.space.chatglm.site",
     ".space.chatglm.site",
   ],
+  experimental: {
+    instrumentationHook: true,
+  },
 };
 
 export default nextConfig;
